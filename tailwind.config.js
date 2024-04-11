1df1dbc0bbabb676d71b8889e010bd94e37253b6
thenaijagamer <sabdullahialaba50@gmail.com>
2024-04-11 13:46:29 +0100

    Add the remaining starter file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,19 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          "marine-blue": "hsl(213, 96%, 18%)",
-          "purplish-blue": "hsl(243, 100%, 62%)",
+          "marine-blue": "hsl(213, 96%, 18%)", // primary text
+          "purplish-blue": "hsl(243, 100%, 62%)", //tertiary text
           "pastel-blue": "hsl(228, 100%, 84%)",
-          "light-blue": "hsl(206, 94%, 87%)",
-          "strawberry-red": "hsl(354, 84%, 57%)",
+          "light-blue": "hsl(206, 94%, 87%)", // step active background
+          "strawberry-red": "hsl(354, 84%, 57%)", // Error color
         },
         neutral: {
-          "cool-gray": "hsl(231, 11%, 63%)",
-          "light-gray": "hsl(229, 24%, 87%)",
-          magnolia: "hsl(217, 100%, 97%)",
-          alabaster: "hsl(231, 100%, 99%)",
+          "cool-gray": "hsl(231, 11%, 63%)", //secondary text
+          "light-gray": "hsl(229, 24%, 87%)", //border
+          magnolia: "hsl(217, 100%, 97%)", // primary background
+          alabaster: "hsl(231, 100%, 99%)", // tertiary background
         },
+        footerLink: "hsl(228, 45%, 44%)",
       },
       fontFamily: {
         ubuntu: ["Ubuntu", "sans-serif"],
