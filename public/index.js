4a630eef4d5542c1a7e5a83acd79300e4ed751b7
thenaijagamer <sabdullahialaba50@gmail.com>
2024-06-24 14:29:45 +0100

    feat: Implement pagination functionality by creating handle page function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,38 @@
 "use strict";
+const nextBtn = document.querySelector(".btn-p--next");
+const prevBtn = document.querySelector(".prev");
+const submitBtn = document.querySelector(".btn-p--confirm");
+let currPage = 1;
+const handlePage = () => {
+    // Page 1, and there are other pages
+    console.log(currPage);
+    if (currPage === 1) {
+        prevBtn.classList.add("invisible");
+        submitBtn.classList.add("hidden");
+        nextBtn.classList.remove("hidden");
+    }
+    if (currPage === 2) {
+        prevBtn.classList.remove("invisible");
+        submitBtn.classList.add("hidden");
+        nextBtn.classList.remove("hidden");
+    }
+    if (currPage === 3) {
+        prevBtn.classList.remove("invisible");
+        submitBtn.classList.add("hidden");
+        nextBtn.classList.remove("hidden");
+    }
+    if (currPage === 4) {
+        prevBtn.classList.remove("invisible");
+        submitBtn.classList.remove("hidden");
+        nextBtn.classList.add("hidden");
+    }
+};
+const handleNext = () => {
+    if (currPage < 4)
+        currPage += 1;
+    handlePage();
+};
+nextBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    handleNext();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
+const nextBtn = document.querySelector(".btn-p--next") as HTMLButtonElement;
+const prevBtn = document.querySelector(".prev") as HTMLButtonElement;
+const submitBtn = document.querySelector(
+  ".btn-p--confirm"
+) as HTMLButtonElement;
 
+let currPage = 1;
+
+const handlePage = (): void => {
+  // Page 1, and there are other pages
+  console.log(currPage);
+  if (currPage === 1) {
+    prevBtn.classList.add("invisible");
+    submitBtn.classList.add("hidden");
+    nextBtn.classList.remove("hidden");
+  }
+  if (currPage === 2) {
+    prevBtn.classList.remove("invisible");
+    submitBtn.classList.add("hidden");
+    nextBtn.classList.remove("hidden");
+  }
+  if (currPage === 3) {
+    prevBtn.classList.remove("invisible");
+    submitBtn.classList.add("hidden");
+    nextBtn.classList.remove("hidden");
+  }
+  if (currPage === 4) {
+    prevBtn.classList.remove("invisible");
+    submitBtn.classList.remove("hidden");
+    nextBtn.classList.add("hidden");
+  }
+};
+
+const handleNext = (): void => {
+  if (currPage < 4) currPage += 1;
+  handlePage();
+};
+
+nextBtn.addEventListener<"click">("click", (e: Event) => {
+  e.preventDefault();
+  handleNext();
+});
