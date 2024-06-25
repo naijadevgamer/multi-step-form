ffeb624d2fd09c68f8388c0ff17010d69970b9b4
thenaijagamer <sabdullahialaba50@gmail.com>
2024-06-25 03:47:12 +0100

    feat: Add form change to handle page functionality

--- public/index.js
+++ public/index.js
@@ -5,49 +5,12 @@ const submitBtn = document.querySelector(".btn-p--confirm");
 const navBtns = document.querySelectorAll(".btn-nav");
 const pagination = document.querySelector(".pagination");
 const sidebar = document.querySelector(".sidebar");
+const infoForm = document.querySelector(".personal-info");
+const planForm = document.querySelector(".plan");
+const addOnForm = document.querySelector(".add-ons");
+const summary = document.querySelector(".summary");
 let currPage = 1;
-const handlePageChange = () => {
-    // Page 1, and there are other pages
-    console.log(currPage);
-    if (currPage === 1) {
-        prevBtn.classList.add("invisible");
-        submitBtn.classList.add("hidden");
-        nextBtn.classList.remove("hidden");
-    }
-    if (currPage === 2) {
-        prevBtn.classList.remove("invisible");
-        submitBtn.classList.add("hidden");
-        nextBtn.classList.remove("hidden");
-    }
-    if (currPage === 3) {
-        prevBtn.classList.remove("invisible");
-        submitBtn.classList.add("hidden");
-        nextBtn.classList.remove("hidden");
-    }
-    if (currPage === 4) {
-        prevBtn.classList.remove("invisible");
-        submitBtn.classList.remove("hidden");
-        nextBtn.classList.add("hidden");
-    }
-};
-const handlePagination = (e) => {
-    const target = e.target;
-    // if (target && target.nodeName === "BUTTON") {
-    e.preventDefault();
-    // Handle next step
-    if (target.classList.contains("btn-p--next") && currPage < 4) {
-        currPage++;
-        handleBtnActive(currPage);
-        handlePageChange();
-    }
-    // Handle prev step
-    if (target.classList.contains("prev") && currPage > 1) {
-        currPage--;
-        handleBtnActive(currPage);
-        handlePageChange();
-    }
-};
-pagination.addEventListener("click", handlePagination);
+// Handle step button active in sidebar
 const handleBtnActive = (num) => {
     navBtns.forEach((e, i) => {
         const el = e;
@@ -57,7 +20,56 @@ const handleBtnActive = (num) => {
             el.classList.remove("btn-nav--active");
     });
 };
-const handleNavButtonClick = (e) => {
+// Change the step of the form
+const handlePageChange = () => {
+    // Page 1, and there are other pages
+    console.log(currPage);
+    // Manipulate pagination display classes
+    submitBtn.classList.add("hidden");
+    nextBtn.classList.remove("hidden");
+    prevBtn.classList.remove("invisible");
+    // Hide all forms
+    infoForm.classList.add("hidden");
+    planForm.classList.add("hidden");
+    addOnForm.classList.add("hidden");
+    summary.classList.add("hidden");
+    // Match changes
+    if (currPage === 1) {
+        prevBtn.classList.add("invisible");
+        infoForm.classList.remove("hidden");
+    }
+    if (currPage === 2)
+        planForm.classList.remove("hidden");
+    if (currPage === 3)
+        addOnForm.classList.remove("hidden");
+    if (currPage === 4) {
+        submitBtn.classList.remove("hidden");
+        nextBtn.classList.add("hidden");
+        summary.classList.remove("hidden");
+    }
+};
+// Handles the step changes through next or prev button click
+const handleStepChange = (e) => {
+    const target = e.target;
+    if (target && target.nodeName === "BUTTON") {
+        e.preventDefault();
+        // Handle next step
+        if (target.classList.contains("btn-p--next") && currPage < 4) {
+            currPage++;
+            handleBtnActive(currPage);
+            handlePageChange();
+        }
+        // Handle prev step
+        if (target.classList.contains("prev") && currPage > 1) {
+            currPage--;
+            handleBtnActive(currPage);
+            handlePageChange();
+        }
+    }
+};
+pagination.addEventListener("click", handleStepChange);
+// Handles the step changes through sidebar nav click
+const handleSidebarClick = (e) => {
     const target = e.target;
     if (target && target.nodeName === "BUTTON") {
         const elNum = target.textContent;
@@ -68,4 +80,4 @@ const handleNavButtonClick = (e) => {
         }
     }
 };
-sidebar.addEventListener("click", handleNavButtonClick);
+sidebar.addEventListener("click", handleSidebarClick);
--- src/index.ts
+++ src/index.ts
@@ -4,58 +4,16 @@ const submitBtn = document.querySelector(
   ".btn-p--confirm"
 ) as HTMLButtonElement;
 const navBtns = document.querySelectorAll(".btn-nav") as NodeList;
-
 const pagination = document.querySelector(".pagination") as HTMLDivElement;
 const sidebar = document.querySelector(".sidebar") as HTMLDivElement;
+const infoForm = document.querySelector(".personal-info") as HTMLDivElement;
+const planForm = document.querySelector(".plan") as HTMLDivElement;
+const addOnForm = document.querySelector(".add-ons") as HTMLDivElement;
+const summary = document.querySelector(".summary") as HTMLDivElement;
 
 let currPage = 1;
 
-const handlePageChange = (): void => {
-  // Page 1, and there are other pages
-  console.log(currPage);
-  if (currPage === 1) {
-    prevBtn.classList.add("invisible");
-    submitBtn.classList.add("hidden");
-    nextBtn.classList.remove("hidden");
-  }
-  if (currPage === 2) {
-    prevBtn.classList.remove("invisible");
-    submitBtn.classList.add("hidden");
-    nextBtn.classList.remove("hidden");
-  }
-  if (currPage === 3) {
-    prevBtn.classList.remove("invisible");
-    submitBtn.classList.add("hidden");
-    nextBtn.classList.remove("hidden");
-  }
-  if (currPage === 4) {
-    prevBtn.classList.remove("invisible");
-    submitBtn.classList.remove("hidden");
-    nextBtn.classList.add("hidden");
-  }
-};
-
-const handlePagination = (e: Event): void => {
-  const target = e.target as HTMLElement;
-  // if (target && target.nodeName === "BUTTON") {
-  e.preventDefault();
-  // Handle next step
-
-  if (target.classList.contains("btn-p--next") && currPage < 4) {
-    currPage++;
-    handleBtnActive(currPage);
-    handlePageChange();
-  }
-  // Handle prev step
-  if (target.classList.contains("prev") && currPage > 1) {
-    currPage--;
-    handleBtnActive(currPage);
-    handlePageChange();
-  }
-};
-
-pagination.addEventListener("click", handlePagination);
-
+// Handle step button active in sidebar
 const handleBtnActive = (num: number) => {
   navBtns.forEach((e, i) => {
     const el = e as HTMLButtonElement;
@@ -64,7 +22,63 @@ const handleBtnActive = (num: number) => {
   });
 };
 
-const handleNavButtonClick = (e: Event) => {
+// Change the step of the form
+const handlePageChange = (): void => {
+  // Page 1, and there are other pages
+  console.log(currPage);
+
+  // Manipulate pagination display classes
+  submitBtn.classList.add("hidden");
+  nextBtn.classList.remove("hidden");
+  prevBtn.classList.remove("invisible");
+
+  // Hide all forms
+  infoForm.classList.add("hidden");
+  planForm.classList.add("hidden");
+  addOnForm.classList.add("hidden");
+  summary.classList.add("hidden");
+
+  // Match changes
+  if (currPage === 1) {
+    prevBtn.classList.add("invisible");
+    infoForm.classList.remove("hidden");
+  }
+  if (currPage === 2) planForm.classList.remove("hidden");
+
+  if (currPage === 3) addOnForm.classList.remove("hidden");
+
+  if (currPage === 4) {
+    submitBtn.classList.remove("hidden");
+    nextBtn.classList.add("hidden");
+    summary.classList.remove("hidden");
+  }
+};
+
+// Handles the step changes through next or prev button click
+const handleStepChange = (e: Event): void => {
+  const target = e.target as HTMLElement;
+  if (target && target.nodeName === "BUTTON") {
+    e.preventDefault();
+    // Handle next step
+    if (target.classList.contains("btn-p--next") && currPage < 4) {
+      currPage++;
+      handleBtnActive(currPage);
+      handlePageChange();
+    }
+
+    // Handle prev step
+    if (target.classList.contains("prev") && currPage > 1) {
+      currPage--;
+      handleBtnActive(currPage);
+      handlePageChange();
+    }
+  }
+};
+
+pagination.addEventListener("click", handleStepChange);
+
+// Handles the step changes through sidebar nav click
+const handleSidebarClick = (e: Event) => {
   const target = e.target as HTMLElement;
   if (target && target.nodeName === "BUTTON") {
     const elNum = target.textContent;
@@ -76,4 +90,4 @@ const handleNavButtonClick = (e: Event) => {
   }
 };
 
-sidebar.addEventListener("click", handleNavButtonClick);
+sidebar.addEventListener("click", handleSidebarClick);
